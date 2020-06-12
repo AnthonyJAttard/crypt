5eeadd6ccd642782a9bdf98f1b57971021383462
anthony <tonyjattard@gmail.com>
2020-06-12 17:17:40 +1000

    Filled out the controller.js functions and modified the mapBuilder.js and mapData.js modules.

--- game.js
+++ game.js
@@ -3,12 +3,13 @@
  * Create the game object
  * */
 
-let playerData = {
-    "name": "Anthony",
-    "health": 50,
-    "items":  [
-        "a rusty key"
-    ]
-}
+// let playerData = {
+//     "name": "Anthony",
+//     "health": 50,
+//     "items":  [
+//         "a rusty key"
+//     ]
+// }
 
-let game = theCrypt.init(theCrypt.mapData, playerData);
+console.log("Loading The Crypt...");
+let game = theCrypt.init(theCrypt.mapData, "Anthony");
